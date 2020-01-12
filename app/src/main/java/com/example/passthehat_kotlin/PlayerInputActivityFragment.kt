af5ebc54f179ef281e607f3bb16d29f47a978e0d
AnasChakfeh <anas.chakfeh@gmail.com>
2020-01-12 19:00:25 +0100

    Number of teams activity Starting with the player input activity

---
+++ app/src/main/java/com/example/passthehat_kotlin/PlayerInputActivityFragment.kt
@@ -0,0 +1,20 @@
+package com.example.passthehat_kotlin
+
+import androidx.fragment.app.Fragment
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+class PlayerInputActivityFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_player_input, container, false)
+    }
+}
